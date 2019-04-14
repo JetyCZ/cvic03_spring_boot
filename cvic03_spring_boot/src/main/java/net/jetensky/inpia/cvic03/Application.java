ef3cb967fe8e0601a52230bd4ea1b8bfccab8125
Pavel Jetensky <pavel.jetensky@hooyu.com>
2019-04-14 16:24:12 +0200

    Cvic06 Geb test with creator

--- cvic03_spring_boot/src/main/java/net/jetensky/inpia/cvic03/Application.java
+++ cvic03_spring_boot/src/main/java/net/jetensky/inpia/cvic03/Application.java
@@ -2,15 +2,18 @@ package net.jetensky.inpia.cvic03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class Application {
 
+    public static ConfigurableApplicationContext context;
+
     public static void main(String[] args) {
         System.out.println(46);
-        SpringApplication.run(Application.class, args);
+        context = SpringApplication.run(Application.class, args);
     }
 
 }
